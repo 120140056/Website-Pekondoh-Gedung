7d76ef9228f49a6d12e4e54824bd7702392a4b86
120140056 <103248575+120140056@users.noreply.github.com>
2023-01-18 23:00:49 +0700

    update

--- assets/js/ollie.js
+++ assets/js/ollie.js
@@ -38,6 +38,10 @@ $('#owl-portfolio').owlCarousel({
     dots: false,
     responsiveClass:true,
     responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
         600:{
             items:3,
             nav:false
